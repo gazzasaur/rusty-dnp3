29a94c5c9e2f9904a42653eb6ada705afd4fdbe2
Gaz <30276587+gazzasaur@users.noreply.github.com>
2026-07-14 22:45:08 +1000

    A lot of playing with the API to get it right

--- rusty-dnp3-api/src/lib.rs
+++ rusty-dnp3-api/src/lib.rs
@@ -1,4 +1,8 @@
+use std::fmt::Debug;
+
 use thiserror::Error;
+
+use crate::DataPointObjectList::{NoRange, StartAndStop};
 
 #[derive(Error, Debug)]
 pub enum RustyDnp3Error {
@@ -15,26 +19,69 @@ pub enum RustyDnp3Error {
     Unknown { reason: String },
 }
 
+fn a() {
+    let b = Dnp3ObjectList::BinaryInputDataPointObject(StartAndStop(RangeSpecifier::OneOctet(0), vec![]));
+    match &b {
+        Dnp3ObjectList::BinaryInputDataPointObject(data_point_object_list) => c(data_point_object_list),
+    };
+}
+
+fn c<'a, T: DataPointObject>(f: &'a DataPointObjectList<T>) -> Option<&'a Vec<IndexedDataPointObject<u8, T>>> {
+    match f {
+        NoRange => return None,
+        StartAndStop(range_specifier, items) => return None,
+        DataPointObjectList::OneOctetIndex(range_specifier_size, indexed_data_point_objects) => return Some(indexed_data_point_objects),
+        DataPointObjectList::TwoOctetIndex(range_specifier_size, indexed_data_point_objects) => return None,
+        DataPointObjectList::FourOctetIndex(range_specifier_size, indexed_data_point_objects) => return None,
+    };
+}
+
+pub enum Dnp3ObjectList {
+    BinaryInputDataPointObject(DataPointObjectList<BinaryInputDataPointObject>),
+}
+
+pub enum RangeSpecifier {
+    OneOctet(u8),
+    TwoOctets(u16),
+    FourOctets(u32),
+}
+
+pub enum RangeSpecifierSize {
+    OneOctet,
+    TwoOctets,
+    FourOctets,
+}
+
+pub enum DataPointObjectList<T: DataPointObject> {
+    NoRange,
+    StartAndStop(RangeSpecifier, Vec<T>),
+
+    OneOctetIndex(RangeSpecifierSize, Vec<IndexedDataPointObject<u8, T>>),
+    TwoOctetIndex(RangeSpecifierSize, Vec<IndexedDataPointObject<u16, T>>),
+    FourOctetIndex(RangeSpecifierSize, Vec<IndexedDataPointObject<u32, T>>),
+}
+
+pub trait DataPointObject {}
+impl DataPointObject for BinaryInputDataPointObject {}
+impl DataPointObject for BinaryInputEventDataPointObject {}
+
+pub trait IndexSize {}
+impl IndexSize for u8 {}
+impl IndexSize for u16 {}
+impl IndexSize for u32 {}
+
+pub struct IndexedDataPointObject<U: IndexSize, T: DataPointObject> {
+    pub index: U,
+    pub data_point: T,
+}
+
 pub enum Dnp3Timestamp {
     None,
     Relative(u64),
     Absolute(u64),
 }
 
-pub enum DataPointValue {
-    BinaryInputDataPoint(BinaryInputDataPointValue),
-    BinaryInputEvent(BinaryInputEventValue),
-
-    DoubleBitBinaryInputDataPoint(DoubleBitBinaryInputDataPointValue),
-    DoubleBitBinaryInputEvenValue(DoubleBitBinaryInputEvent),
-}
-
-pub struct IndexedDataPoint<T> {
-    pub index: u32,
-    pub data_point: T
-}
-
-pub struct BinaryInputDataPointValue {
+pub struct BinaryInputDataPointObject {
     pub state: bool,
     pub chatter_filter: bool,
     pub local_forced: bool,
@@ -44,8 +91,8 @@ pub struct BinaryInputDataPointValue {
     pub online: bool,
 }
 
-pub struct BinaryInputEventValue {
-    pub data_point: BinaryInputDataPointValue,
+pub struct BinaryInputEventDataPointObject {
+    pub data_point: BinaryInputDataPointObject,
     pub timestamp: Dnp3Timestamp,
 }
 
@@ -56,7 +103,7 @@ pub enum DoubleBitBinaryState {
     Abnormal,   // 3: Abnormal or Custom
 }
 
-pub struct DoubleBitBinaryInputDataPointValue {
+pub struct DoubleBitBinaryInputDataPointObject {
     pub state: DoubleBitBinaryState,
     pub chatter_filter: bool,
     pub local_forced: bool,
@@ -66,7 +113,7 @@ pub struct DoubleBitBinaryInputDataPointValue {
     pub online: bool,
 }
 
-pub struct DoubleBitBinaryInputEvent {
-    pub data_point: DoubleBitBinaryInputDataPointValue,
+pub struct DoubleBitBinaryInputEventDataPointObject {
+    pub data_point: DoubleBitBinaryInputDataPointObject,
     pub timestamp: Dnp3Timestamp,
 }
